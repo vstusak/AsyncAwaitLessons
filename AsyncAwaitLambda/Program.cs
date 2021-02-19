f1aa89334ca474049a226993e140cd2cf8f92996
Josef Rudolf <Josef.Rudolf@rws.com>
2021-02-19 08:29:22 +0100

    Async Await Lambda

---
+++ AsyncAwaitLambda/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace AsyncAwaitLambda
+{
+    internal class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            await TimeWrapper(async () =>
+            {
+                await Workload();
+            });
+            Console.ReadKey();
+        }
+
+        private static async Task TimeWrapper(Func<Task> a)
+        {
+            Console.WriteLine($"{DateTime.Now}");
+            await a();
+            Console.WriteLine($"{DateTime.Now}");
+        }
+
+        private static async Task Workload()
+        {
+            Console.WriteLine("Workload started.");
+            await Task.Delay(5000).ConfigureAwait(false);
+            Console.WriteLine("Workload finished.");
+        }
+    }
+}
