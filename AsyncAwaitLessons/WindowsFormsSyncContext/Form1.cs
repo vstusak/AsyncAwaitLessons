101bc55c288b2c3716f4fc243dd06084af3f71bb
Tomáš Fulajtár <tomas.fulajtar@rws.com>
2021-03-10 16:14:37 +0100

    Windows Forms project added with  example of  ConfiguraAwait.

---
+++ AsyncAwaitLessons/WindowsFormsSyncContext/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsSyncContext
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            //SynchronizationContext.Current
+            //Thread.Sleep(5000);
+
+            await Task.Delay(5000).ConfigureAwait(false);
+            
+            button1.Text = "Hello Dolly";
+
+        }
+    }
+}
