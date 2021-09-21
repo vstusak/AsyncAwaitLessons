67b09d0c2dbed3b7d52d49dc0671201b1b9642b8
Petr Jankovsky <petr.jankovsky@rws.com>
2021-09-21 09:21:28 +0200

    AsyncAwaitLessons2

--- AsyncAwaitLessonsDopeMob/AsyncAwaitLessons/Program.cs
+++ AsyncAwaitLessonsDopeMob/AsyncAwaitLessons/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace AsyncAwaitLessons
@@ -10,6 +12,17 @@ namespace AsyncAwaitLessons
     {
         static void Main(string[] args)
         {
+            Task t = Task.Run(() =>
+            {
+                Console.WriteLine("Hello world.");
+                Thread.Sleep(1000);
+                FileStream fileStream = new FileStream("", FileMode.OpenOrCreate);
+                //await fileStream.ReadAsync();
+
+                //TODO - řádek 38 - pokračovat
+            });
+
+            Console.ReadLine();
         }
     }
 }
